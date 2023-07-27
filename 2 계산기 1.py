45d58a1fdaa2ea41746da04904718d8aff49e220
SnU <singularunconventional@gmail.com>
2023-07-27 17:31:11 +0900

    20230727 Calculator update

--- 2 계산기 1.py
+++ 2 계산기 1.py
@@ -1,8 +1,22 @@
-"""
-앞서 배운 내용을 바탕으로 간단한 계산 프로그램을 만들것이다.
-int()는 '변수의 형식' 참고
-"""
+Num1 = 0
+Num2 = 0
+arith = 0
+out = 0
 
-Num1 = int(input("처음 값 : "))
-Num2 = int(input("다음 값 : "))
+while True:
+    Num1 = int(input("처음 값을 입력하세요 : "))
+    Num2 = int(input("나중 값을 입력하세요 : "))
 
+    print("-(빼기), +(더하기), x(곱하기), /(나누기)")
+    arith = int(input("원하는 작업을 선택하세요 : "))
+    
+    if (arith == '+'):      out = (Num1 + Num2)
+    elif (arith == '-'):    out = (Num1 - Num2)
+    elif (arith == 'x'):    out = (Num1 * Num2)
+    elif (arith == '/'):    out = (Num1 / Num2)
+
+    else:
+        print("값이 잘못되었습니다. 다시 입력하세요.")
+        out = ''
+
+    print(out)
